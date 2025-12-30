f3aff9fb310e267e20fc7562a1c902e6a200ce28
kubealex <al.rossi87@gmail.com>
2025-12-30 12:18:30 +0100

    test

--- main.tf
+++ main.tf
@@ -24,23 +24,21 @@ module "test_pool" {
 module "test_network" {
   source = "../terraform-libvirt-libvirt-resources/modules/terraform-libvirt-network"
 
-  network_name       = "test-network"
-  network_mode       = "nat"
-  network_domain     = "test.local"
-  network_cidr       = ["192.168.10.0/24"]
-  network_autostart  = true
-  network_dns_enabled = true
-  network_dns_local   = true
-  network_dhcp_enabled = true
+  network_name             = "test-network"
+  network_mode             = "nat"
+  network_domain           = "test.local"
+  network_cidr             = ["192.168.10.0/24"]
+  network_autostart        = true
+  network_dns_enabled      = true
+  network_dns_local        = true
+  network_dhcp_enabled     = true
   network_dhcp_range_start = "192.168.10.10"
   network_dhcp_range_end   = "192.168.10.100"
 
-  # DNS entries for the test VM
   network_dns_entries = {
     "test-vm" = "192.168.10.50"
   }
 
-  # Optional: Add a DNS SRV record
   network_dns_srv_records = [
     {
       service  = "http"
@@ -53,56 +51,55 @@ module "test_network" {
     }
   ]
 
-  network_routes = {
-    # Optional: Add custom routes if needed
-    # "10.0.0.0/24" = "192.168.10.1"
-  }
+  network_routes = {}
 }
 
 # Create an instance
 module "test_instance" {
   source = "../terraform-libvirt-libvirt-resources/modules/terraform-libvirt-instance"
 
-  # Dependencies
+  # Dependencies ensure modules are ready before instance creation
   depends_on = [
     module.test_pool,
     module.test_network
   ]
 
-  instance_count              = 1
-  instance_hostname           = "test-vm"
-  instance_domain             = "test.local"
-  instance_autostart          = true
-  instance_cpu                = 2
-  instance_memory             = 2  # 2 GB
-  instance_volume_size        = 10 # 10 GB
-  instance_type               = "linux"
-  instance_libvirt_pool       = module.test_pool.pool_name
-  instance_uefi_enabled       = true
-  instance_firmware           = "/usr/share/edk2/ovmf/OVMF_CODE.fd"
+  instance_count        = 1
+  instance_hostname     = "test-vm"
+  instance_domain       = "test.local"
+  instance_autostart    = true
+  instance_cpu          = 2
+  instance_memory       = 2  # Converted to GB in module (var.instance_memory * 1024)
+  instance_volume_size  = 10 # GB
+  instance_type         = "linux"
 
-  # Use Ubuntu cloud image
+  # References the pool name exported by the pool module
+  instance_libvirt_pool = module.test_pool.pool_name
+
+  instance_uefi_enabled = true
+  instance_firmware     = "/usr/share/edk2/ovmf/OVMF_CODE.fd"
+
   instance_cloud_image = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
 
-  # Cloud-init configuration
   instance_cloudinit_path = "${path.module}/cloud-init.cfg"
 
+  # Matches the 'object' type defined in your variables
   instance_cloud_user = {
     username = "ubuntu"
     password = "ubuntu"
-    sshkey   = file("~/.ssh/id_rsa.pub")  # Replace with your SSH public key path
+    sshkey   = file("~/.ssh/id_rsa.pub")
   }
 
-  # Network configuration
+  # Matches the 'list(object)' type defined in your variables
   instance_network_interfaces = [
     {
       interface_network     = module.test_network.network_name
       interface_mac_address = "52:54:00:ab:cd:01"
       interface_addresses   = ["192.168.10.50"]
       interface_hostname    = "test-vm"
+      # interface_wait_for_lease is optional, defaults to true
     }
   ]
 
-  # Optional: Add additional storage
-  instance_additional_volume_size = 5  # 5 GB additional disk
+  instance_additional_volume_size = 5
 }
